29c7ae48e6b399083a032f984ef13dfe11a92bcc
OliverMengich <kemeioliver2000@gmail.com>
2023-04-12 21:46:12 +0300

    Restructures the footer component

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -4,17 +4,14 @@ import { FaTwitter } from "@react-icons/all-files/fa/FaTwitter";
 import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
 import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
 import { FaEnvelope } from "@react-icons/all-files/fa/FaEnvelope";
-import { FaPhone } from "@react-icons/all-files/fa/FaPhone";
-import { FaFilePdf } from "@react-icons/all-files/fa/FaFilePdf";
-import { FaWhatsapp }from "@react-icons/all-files/fa/FaWhatsapp";
-import {FaLocationArrow} from "@react-icons/all-files/fa/FaLocationArrow";
 const footerStyle = {
     backgroundColor: "#05242a",
-    color: "white",
+    borderTop:'2px solid #ccc',
+    color: "inherit",
     padding: "20px 0",
     display: "flex",
+    flexWrap: "wrap",
     justifyContent: "center",
-    // alignItems: "center",
 };
 const FooterStyleUl = styled.ul`
     list-style: none;
@@ -22,11 +19,9 @@ const FooterStyleUl = styled.ul`
         font-size: 10px;
     }
 `;
-const footerStyleLi = {
-
-}
 const footerSection = {
-    width: '30%'
+    // minWidth: '250px'
+    margin: '0'
 }
 const titleSec = {
     fontSize: '1.5rem',
@@ -36,12 +31,12 @@ const titleSec = {
 }
 const SocialMedia = styled.div`
     display: flex;
-    color: white;
+    color: inherit;
     width: 50%;
     text-align: center;
     margin: 1rem 0;
     align-content: center;
-}`;
+`;
 
 const SocialMediaA = styled.a`
     height: 35px;
@@ -50,62 +45,54 @@ const SocialMediaA = styled.a`
     font-size: 1.5rem;
     margin: .5rem 0;
     border-radius: 50%;
-    color: white;
+    color: inherit;
     text-decoration: none;
     margin: 0 .5rem;
     padding: auto;
-}`
+`
 const NavMediaA = styled.a`
     cursor: pointer;
     margin: .5rem 0;
-    color: white;
+    color: inherit;
     text-decoration: none;
     margin: 0 .5rem;
     padding: auto;
-}`
-export default function Footer(){
+`
+type Props={
+    theme?:{
+        backgroundColor: string,
+        color: string,
+        aboutMeTheme: {
+            backgroundColor: string,
+        },
+    }
+}
+export default function Footer({theme}: Props){
+    var date = new Date();
+    const days =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    const day=days[date.getDay()]
     return(
-        <footer style={footerStyle}>
+        <footer style={{borderTop:'2px solid #ccc',
+        backgroundColor: theme?.backgroundColor==='white'? "white": '#05242a',
+        color: theme?.color,
+        padding: "20px 0",
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",}}>
             <div style={footerSection}>
-                <h4 style={titleSec}>Oliver Kipkemei</h4>
+                <SocialMedia>
+                    <SocialMediaA href="https://twitter.com/OllieKem7">
+                        <FaTwitter/>
+                    </SocialMediaA>
+                    <SocialMediaA href="https://github.com/OliverMengich"><FaGithub/></SocialMediaA>
+                    <SocialMediaA href="https://www.linkedin.com/in/oliver-arsenal"> <FaLinkedin/> </SocialMediaA>
+                    <SocialMediaA href="mailto:oliverkemei2000@gmail.com"> <FaEnvelope/> </SocialMediaA>
+                </SocialMedia>
+            </div>
+            <div style={footerSection}>
                 <p>
-                    I'm a software developer based in Nairobi, Kenya. I'm
-                    passionate about building software that solves real
-                    problems.
+                    Oliver Kipkemei | Have a good {day}
                 </p>
-                    <SocialMedia>
-                        <SocialMediaA href="https://twitter.com/OllieKem7">
-                            <FaTwitter/>
-                        </SocialMediaA>
-                        <SocialMediaA href="https://github.com/OliverMengich"><FaGithub/></SocialMediaA>
-                        <SocialMediaA href="https://www.linkedin.com/in/oliver-arsenal"> <FaLinkedin/> </SocialMediaA>
-                        <SocialMediaA href="mailto:oliverkemei2000@gmail.com"> <FaEnvelope/> </SocialMediaA>
-                    </SocialMedia>
-            </div>
-            <div style={footerSection}>
-                <h4 style={titleSec}>Navigation</h4>
-                <FooterStyleUl>
-                    <li>
-                        <NavMediaA href="/"> <FaLocationArrow/> Home</NavMediaA>
-                    </li>
-                    <li>
-                        <NavMediaA href="/about"> <FaLocationArrow/> About</NavMediaA>
-                    </li>
-                    <li>
-                        <NavMediaA href="/blogs"> <FaLocationArrow/> Blogs</NavMediaA>
-                    </li>
-                    <li>
-                        <NavMediaA href="/contact"> <FaLocationArrow/> Contact</NavMediaA>
-                    </li>
-                </FooterStyleUl>
-            </div>
-            <div style={footerSection}>
-                <h4 style={titleSec}>Contact Me</h4>
-                <FooterStyleUl>
-                    <li> <FaPhone/> +(254)-741-954-425</li>
-                    <li> <FaWhatsapp/> </li>    
-                    <li> <FaFilePdf/> Download Resume </li>    
-                </FooterStyleUl>
             </div>
         </footer>
     )
