385f44da4348c9d8b3f17f3696b7477156acb8ba
OliverMengich <kemeioliver2000@gmail.com>
2023-05-21 15:09:09 +0300

    Changes in blog page styling and content render

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -5,6 +5,9 @@ import { Link } from "gatsby";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import remarkGfm from "remark-gfm";
 import TagListComponent from "../components/TagList.component";
+import styled from "styled-components";
+import { marked } from "marked";
+import Footer from "../components/Footer/Footer.component";
 type Props = {
     data: {
         devArticles: {
@@ -38,18 +41,82 @@ type Props = {
             photo: string
         }
     }
-}
+};
+const BlogMarkdown = styled.pre`
+    max-width: 768px;
+    overflow-x: hidden;
+    flex-wrap: wrap;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    border-radius: 5px;
+    font-size: 1.1em;
+    font-family: monospace;
+    color: #000;
+    margin: 0 5px;
+    @media screen and (max-width: 768px) {
+        overflow-x: hidden;
+        width: 95vw;
+    }
+    pre{
+        flex-direction: column;
+        justify-content: center;
+        flex-wrap: wrap;
+        padding: 10px;
+        width: 100%;
+        background-color: rgb(38, 38, 38);
+        overflow-x: auto;
+        border-radius: 5px;
+        font-size: 10px;
+        font-family: monospace;
+        color: #000;
+        margin: 0 5px;
+        @media (max-width: 768px) {
+            font-size: 10px;
+        }
+        code{
+            padding: 0;
+            background-color: transparent;
+            border-radius: 0;
+            font-family: monospace;
+            color: rgb(127, 219, 202);
+            margin: 0;
+        }
+    }
+    p{
+        padding: 10px;
+        font-family: monospace;
+        color: #000;
+        margin: 0 5px;
+        width: 100%;
+        code{
+            padding: 5px;
+            background-color: #ccc;
+            border-radius: 5px;
+            font-family: monospace;
+            color: inherit;
+            margin: 0;
+        }
+    }
+`;
 export default function BlogPost({ data }: Props) {
     const { devArticles: { article },hashNodeUser,hashNodePost } = data;
+    const markdown = marked(article.body_markdown|| '#No Content');
     return (
         <Layout>
             <div style={{
-                width:'100%'
+                width:'100%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: '0 10px',
+                boxSizing: 'border-box',
             }}>
                 <h1>{article.title}</h1>
                 <h3 style={{color: '#ccc'}}>{new Date(article.published_at || hashNodePost?.dateAdded).toDateString()}</h3>
                 <div style={{borderBottom: '1px solid #ccc',}}>
-                    <div style={{width: '100%',textAlign: 'center',display: 'flex',justifyContent: 'center',alignItems: 'center',}}>
+                    <div style={{ boxSizing: 'border-box', width: '100%',textAlign: 'center',display: 'flex',justifyContent: 'center',alignItems: 'center',}}>
                         {
                             article && <img style={{ width: '50px', height: '50px',borderRadius: '50%',}}src={article.user.profile_image} alt={article.user.name} />
                         }
@@ -63,26 +130,24 @@ export default function BlogPost({ data }: Props) {
                             </Link>
                         </div>
                     </div>
-                    <h3 style={{color: '#ccc'}}>TAGS</h3>
-                    <TagListComponent tags={article.tag_list} />
-                    <Link style={{
-                        display: 'inline-block',
-                        textTransform: 'uppercase',
-                        padding: '0.5rem',
-                        fontWeight: 700,
-                        color: '#007e6a',
-                        fontSize: '1.2rem',
-                        textDecoration: 'none',
-                        }} to={`/blogs`}>
-                        Back To Blog &rarr;
-                    </Link>
+                    <div style={{ boxSizing: 'border-box', width: '100%',textAlign: 'center',display: 'flex',justifyContent: 'center',alignItems: 'center',}}>
+                        <h3 style={{color: '#ccc', margin: 0,padding: 0}}>TAGS</h3>
+                        <TagListComponent tags={article.tag_list} />
+                        <Link style={{
+                            display: 'inline-block',
+                            color: '#007e6a',
+                            fontSize: 'calc(10px + 0.5vw)',
+                            textDecoration: 'none',
+                            }} to={`/blogs`}>
+                            &larr; Back To Blogs 
+                        </Link>
+                    </div>
                 </div>
-                {
-                    article.body_markdown && <ReactMarkdown rehypePlugins={[remarkGfm]}>
-                        {article.body_markdown}
-                    </ReactMarkdown>
-                }
+                {/* <div style={{width: '100%', boxSizing: 'border-box',}}> */}
+                    <BlogMarkdown dangerouslySetInnerHTML={{__html: markdown}} />
+                {/* </div> */}
             </div>
+            <Footer />
         </Layout>
     )
 }
--- src/templates/hashnode-blogpost.tsx
+++ src/templates/hashnode-blogpost.tsx
@@ -2,6 +2,66 @@ import React from "react";
 import { graphql, type HeadFC, type PageProps } from "gatsby";
 import Layout from "../components/layout";
 import { Link } from "gatsby";
+import { marked } from "marked";
+import styled from "styled-components";
+import Footer from "../components/Footer/Footer.component";
+const BlogMarkdown = styled.pre`
+    max-width: 768px;
+    overflow-x: hidden;
+    flex-wrap: wrap;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    border-radius: 5px;
+    font-size: 1.1em;
+    font-family: monospace;
+    color: #000;
+    margin: 0 5px;
+    @media screen and (max-width: 768px) {
+        overflow-x: hidden;
+        width: 95vw;
+    }
+    pre{
+        flex-direction: column;
+        justify-content: center;
+        flex-wrap: wrap;
+        padding: 10px;
+        width: 100%;
+        background-color: rgb(38, 38, 38);
+        overflow-x: auto;
+        border-radius: 5px;
+        font-size: 10px;
+        font-family: monospace;
+        color: #000;
+        margin: 0 5px;
+        @media (max-width: 768px) {
+            font-size: 10px;
+        }
+        code{
+            padding: 0;
+            background-color: transparent;
+            border-radius: 0;
+            font-family: monospace;
+            color: rgb(127, 219, 202);
+            margin: 0;
+        }
+    }
+    p{
+        padding: 10px;
+        font-family: monospace;
+        color: #000;
+        margin: 0 5px;
+        width: 100%;
+        code{
+            padding: 5px;
+            background-color: #ccc;
+            border-radius: 5px;
+            font-family: monospace;
+            color: inherit;
+            margin: 0;
+        }
+    }
+`;
 type Props = {
     data: {
         hashNodePost: {
@@ -22,6 +82,7 @@ type Props = {
 }
 export default function BlogPost({ data }: Props) {
     const { hashNodePost: { childMarkdownRemark:{html},dateAdded, title }, hashNodeUser:{name,photo,username} } = data;
+    const htmlContent = marked(html);
     return (
         <Layout>
             <div style={{display:'flex',alignItems:'center',justifyContent: 'center', width: '100%', flexWrap: 'wrap'}}>
@@ -41,8 +102,9 @@ export default function BlogPost({ data }: Props) {
                         </div>
                     </div>
                 </div>
-                <div style={{width: '100%',minWidth: '500px'}} dangerouslySetInnerHTML={{ __html: html }} />
+                <BlogMarkdown dangerouslySetInnerHTML={{ __html: htmlContent }} />
             </div>
+            <Footer />
         </Layout>
     )
 }
